389c3fac3152589767fa961e95ed026d201757cf
MarcosF767 <marfer737@gmail.com.com>
2021-11-05 19:17:00 -0400

    first commit

---
+++ confundo/common.py
@@ -0,0 +1,13 @@
+# -*- Mode: python; py-indent-offset: 4; indent-tabs-mode: nil; coding: utf-8; -*-
+# Copyright 2019 Alex Afanasyev
+#
+
+MTU=412
+MAX_SEQNO = 40000
+RETX_TIME = 0.5
+FIN_WAIT_TIME = 2.0
+SYN_WAIT_TIME = 10.0
+
+INIT_SSTHRESH = 12000
+
+GLOBAL_TIMEOUT = 10.0
